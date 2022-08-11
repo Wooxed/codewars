1f33ffc661bd1f339575c8a30e28529c0540c79e
Sergei <samkorbut@gmail.com>
2022-08-11 17:30:41 +0400

    Change Is a number prime?

--- codewars022.js
+++ codewars022.js
@@ -1,9 +1,4 @@
 function isPrime(num) {
-  if (num < 2) return false;
-  for (var i = 2; i < num; i++) {
-    if (num % i == 0) {
-      return false;
-    }
-  }
-  return true;
+  for (let i = 2; i < num; i++) if (num % i === 0) return false;
+  return num > 1;
 }
