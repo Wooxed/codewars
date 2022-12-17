c7b2089b47a15323f6d20d5a6e42ebdd96503844
Sergei <samkorbut@gmail.com>
2022-12-17 18:50:05 +0400

    Add Find heavy ball - level: novice

---
+++ codewars153.js
@@ -0,0 +1,15 @@
+function findBall(scales) {
+    for (let i = 1; i < 8; i++) {
+        let leftPan = [i - 1];
+        let rightPan = [8 - i];
+        let w = scales.getWeight(leftPan, rightPan);
+
+        if (w === -1) {// left pan is heavier
+            return leftPan[0];
+        }
+
+        if (w === 1) {// right pan is heavier
+            return rightPan[0];
+        }
+    }
+}
