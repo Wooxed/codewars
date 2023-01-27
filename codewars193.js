547d0ae30f7f726fcce7cbf5a148fd7b13b75753
Sergei <samkorbut@gmail.com>
2023-01-27 10:26:15 +0400

    Add Number of Divisions (second solution)

--- codewars193.js
+++ codewars193.js
@@ -5,3 +5,5 @@ const divisions = (n, divisor, result = 0) => {
     return divisions(n / divisor, divisor, result + 1)
 
 };
+
+const divisions2 = (n, divisor) => Math.floor(Mat.log(n) / Math.log(divisor))
