04890906b65311dea25aab692f64ac8b3aff96eb
Sergei <samkorbut@gmail.com>
2023-02-14 10:45:13 +0400

    Add You're a square!

---
+++ codewars209.js
@@ -0,0 +1,13 @@
+var isSquare = function (n) {
+    if (n < 0) {
+        return false
+    }
+    for (let i = 0; i <= n; i++) {
+        if (i ** 2 === n) {
+            return true
+        } else {
+            continue
+        }
+    }
+    return false
+}
