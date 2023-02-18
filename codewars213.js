335e398a038058353886f1e44db81d897309a15f
Sergei <samkorbut@gmail.com>
2023-02-18 11:30:24 +0400

    Add New £5 notes collectors!

---
+++ codewars213.js
@@ -0,0 +1,10 @@
+function getNewNotes(salary, bills) {
+    for (let i = 0; i < bills.length; i++) {
+        salary -= bills[i]
+    }
+    if (salary < 0) {
+        return 0
+    }
+    return salary / 5 | 0
+}
+
