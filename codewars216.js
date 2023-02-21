46577db623d62baad4d14f5ad562ee11136df34a
Sergei <samkorbut@gmail.com>
2023-02-21 22:29:46 +0400

    Add Number of Rectangles in a Grid

---
+++ codewars216.js
@@ -0,0 +1,9 @@
+function numberOfRectangles(m, n) {
+    let result = 0
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            result += i * j
+        }
+    }
+    return result
+}
