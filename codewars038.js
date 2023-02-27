d4f27085d2f986afedbb28408913161fabbb25f8
Sergei <samkorbut@gmail.com>
2023-02-27 07:29:30 +0400

    Add Get the mean of an array (second solution)

--- codewars038.js
+++ codewars038.js
@@ -1,3 +1,10 @@
 function getAverage(marks) {
   return Math.floor(marks.reduce((acc, cur) => acc + cur) / marks.length);
 }
+
+
+const getAverage2 = marks => {
+  let acc = 0;
+  marks.forEach(el => acc += el)
+  return Math.floor(acc / marks.length)
+}
