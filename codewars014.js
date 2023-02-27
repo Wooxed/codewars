c761bb4ddc2adfff419f91abf95af5547ca9ccde
Sergei <samkorbut@gmail.com>
2023-02-27 07:23:13 +0400

    Add Find the smallest integer in the array (second solution)

--- codewars014.js
+++ codewars014.js
@@ -3,3 +3,13 @@ class SmallestIntegerFinder {
     return Math.min(...args);
   }
 }
+
+class SmallestIntegerFinder2 {
+  findSmallestInt(args) {
+    let currentMin = args[0];
+    for (let i = 1; i < args.length; i++) {
+      if (args[i] < currentMin) currentMin = args[i]
+    }
+    return currentMin
+  }
+}
