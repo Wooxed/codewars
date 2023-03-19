f26249a74df37ae027aac22418da628fb27a7f8e
Sergei <samkorbut@gmail.com>
2023-03-19 07:10:41 +0400

    Add Geometry Basics: Triangle Perimeter in 2D

---
+++ codewars234.js
@@ -0,0 +1,4 @@
+const trianglePerimeter = triangle =>
+    Math.hypot(triangle.a.x - triangle.b.x, triangle.a.y - triangle.b.y) +
+    Math.hypot(triangle.a.x - triangle.c.x, triangle.a.y - triangle.c.y) +
+    Math.hypot(triangle.b.x - triangle.c.x, triangle.b.y - triangle.c.y)
